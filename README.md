c7cbaae29eff29609ee43d5c8a9462f578d372e4
Fudmottin <Fudmottin@users.noreply.github.com>
2024-03-01 04:31:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Chudnovsky Algorithm for Calculating Pi
+
+This program calculates the value of Pi using the [Chudnovsky algorithm](https://en.wikipedia.org/wiki/Chudnovsky_algorithm),
+an extremely fast method to calculate the digits of π. It was published by the Chudnovsky brothers in 1987.
+
+## Prerequisites
+
+The program requires two libraries:
+
+1. **Boost:** A set of peer-reviewed portable C++ source libraries that extend the functionality of C++. You can download it from [here](https://www.boost.org/users/download/). 
+2. **MPFR:** A library for multiple-precision floating-point computations with correct rounding. You can download it from [here](http://www.mpfr.org/mpfr-current/#download).
+
+Make sure to install these libraries before compiling and running the program.
+
+## How To Compile The Program?
+You need a compiler that supports C++20 standard because this code uses features introduced in this version.
+Here is an example compile line: `c++ -std=c++20 -O3 -o chudnovsky chudnovsky.cpp -L/usr/local/opt/mpfr/lib/ -lmpfr`
+
+## Usage
+To run the program, you should pass one argument which is the number of terms to be computed in the series.
+Usage example: `./chudnovsky 10` 
+
+It will compute Pi up to 10 terms.
+
+The output will display Pi computed up to `n` terms (where `n` was your input) and then print out a calculated approximation of Pi.
+
+## Algorithm Explanation
+The Chudnovky algorithm calculates 1/π as:
+
+12 Σ ((-1)^k * (6k)! * (545140134k + 13591409)) / ((3k)!(k!)^3 * (-640320)^(3k+3/2))
+
+Where the summation Σ is from k = 0 to ∞.
+
+The program uses Boost's multiprecision and special function libraries to calculate factorials and powers. It also uses MPFR for multiple-precision floating-point computations.
+
+## Note
+
+Remember that the more terms you use, the more accurate your calculation will be, but it will also take longer to compute. For instance, using 10 terms provides a very accurate approximation of Pi.
+
+This program is a great way to understand how numerical methods work and how C++ can be used for scientific computing tasks. Feel free to experiment with it and optimize it as needed!
+
---
+++ chudnovsky.cpp
@@ -0,0 +1,112 @@
+/* Comput π using the Chudnovsky Algorithm:
+ *
+ * 1/π = 12 Σ ((-1)^k * (6k)! * (545140134k + 13591409)) / ((3k)!(k!)^3 * (-640320)^(3k+3/2))
+ *
+ * Where k = 0 => ∞
+ *
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/math/constants/constants.hpp>
+#include <boost/math/special_functions/factorials.hpp>
+#include <boost/multiprecision/mpfr.hpp>
+
+
+boost::multiprecision::cpp_int factorial(uint32_t num) {
+    using boost::multiprecision::cpp_int;
+    cpp_int fact = 1;
+    for(cpp_int i = 1; i <= num; ++i)
+        fact *= i;
+    return fact;
+}
+
+boost::multiprecision::cpp_int numerator(uint32_t k) {
+    using boost::multiprecision::cpp_int;
+    
+    auto six_k_fact = factorial(6*k);
+    
+    return (k & 1 ? -1 : 1) * six_k_fact * (545140134*k + 13591409);
+}
+
+boost::multiprecision::cpp_int denominator_a(uint32_t k) {
+    auto factorial_k = factorial(k);
+    return factorial(3*k) * factorial_k * factorial_k * factorial_k;
+}
+
+boost::multiprecision::cpp_int pow_3k(uint32_t k) {
+    boost::multiprecision::cpp_int base = 640320;
+    boost::multiprecision::cpp_int ret = 1;
+    uint32_t exponent = 3 * k;
+
+    for (uint32_t i = 0; i < exponent; ++i)
+        ret *= base;
+
+    if ((k & 1) == 1) ret = 0 - ret;
+
+    return ret;
+}
+
+int calcPrecision(int num_terms) {
+    const int MIN_PRECISION = 6; // At least six significant figures.
+    const int PLACES_PER_TERM = 14; // Each term adds about 14 correct decimal places.
+
+    int estimated_precision = num_terms * PLACES_PER_TERM;
+
+    // Ensure at least minimum precision.
+    if (estimated_precision < MIN_PRECISION) {
+        return MIN_PRECISION;
+    }
+
+    return estimated_precision;
+}
+
+// Function to calculate (640320)^(3/2) with desired precision
+boost::multiprecision::mpfr_float calcConstant(int precision){
+    using boost::multiprecision::mpfr_float;
+    mpfr_float::default_precision(precision);
+    mpfr_float result = sqrt(pow(mpfr_float(640320), 3));
+    return result;
+}
+
+
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " number_of_terms\n";
+        return 1;
+    }
+
+    // Ensure input is valid
+    try {
+        uint32_t num_terms = std::stoi(argv[1]);
+        
+        if (num_terms <= 0) {
+            throw std::invalid_argument("Number of terms must be greater than zero.");
+        }
+ 
+        int precision = calcPrecision(num_terms);
+        boost::multiprecision::mpfr_float constant = calcConstant(precision);
+        
+        using boost::multiprecision::mpfr_float;
+      
+        mpfr_float pi_inverse = 0; // Initialize inverse of Pi
+          
+        for(uint32_t k=0; k < num_terms; ++k) {
+            pi_inverse += mpfr_float(numerator(k)) / (mpfr_float(denominator_a(k)) * mpfr_float(pow_3k(k)) * constant);
+        }
+          
+        mpfr_float pi = 1/(pi_inverse * 12);
+        
+        std::cout << "Pi computed up to " << num_terms << " terms is: ";
+        std::cout << std::setprecision(precision) << pi << "\n";
+    } catch(std::exception& e){
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
+    }
+
+    return 0;
+}
+
